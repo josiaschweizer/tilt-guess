863adcdc9172b78bfb2c556ba298bedabfbcd359
josiaschweizer <josia.schweizer@gmail.com>
2026-02-17 13:36:12 +0100

    chore: updated tailwind config

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,5 @@
 import { Text, View } from 'react-native'
 
 export default function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-white text-xl font-bold text-center">
-        hello world
-      </Text>
-    </View>
-  )
+  return <View></View>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './App.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
-  presets: [require("nativewind/preset")],
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: '#000000',
+        background: '#EEE0CB',
+        surface: '#BAA898',
+        primary: '#839788',
+        accent: '#BFD7EA',
+        'on-primary': '#EEE0CB',
+        'on-accent': '#000000',
+      },
+    },
   },
   plugins: [],
 }
