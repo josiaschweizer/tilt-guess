a44f1be13720e1690465b219185e32d33997910d
josiaschweizer <josia.schweizer@gmail.com>
2026-02-18 13:00:15 +0100

    feat: added tilt detection

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -35,6 +35,13 @@ export default function Home() {
             onPress={() => router.push('/history')}
             icon={<History size={20} color="#000000" />}
           />
+
+          <AppButton
+            text="Game"
+            variant="secondary"
+            onPress={() => router.push('/game/play/123')}
+            icon={<History size={20} color="#000000" />}
+          />
         </View>
       </View>
     </View>
---
+++ src/types/tilt/TiltDirection.ts
@@ -0,0 +1,1 @@
+export type TiltDirection = 'forward' | 'backward' | null
